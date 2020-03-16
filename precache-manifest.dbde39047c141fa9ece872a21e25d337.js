4ec95042b769dae39dc737614755e3c8bd71a3d1
John Muir <johnmuir1907@gmail.com>
2020-03-16 12:12:58 +0000

    Updates

---
+++ precache-manifest.dbde39047c141fa9ece872a21e25d337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e6da0f51eab36fe32bf67322d72404f",
+    "url": "/todolist/index.html"
+  },
+  {
+    "revision": "fecb8b065344a82e313e",
+    "url": "/todolist/static/css/main.3d9fc41a.chunk.css"
+  },
+  {
+    "revision": "0fcb0f589dcf48ca563a",
+    "url": "/todolist/static/js/2.69298e74.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist/static/js/2.69298e74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fecb8b065344a82e313e",
+    "url": "/todolist/static/js/main.18409e17.chunk.js"
+  },
+  {
+    "revision": "cebccde0f7f76a903cc9",
+    "url": "/todolist/static/js/runtime-main.6fa7f924.js"
+  }
+]);
